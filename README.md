37e94d848bcbb690699e8504d52de93238385d4d
calvinkawai <ljw361@hotmail.com>
2021-07-07 15:20:57 -0400

    add demo and complete tutorial doc

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# trivia-vuejs
+# trivia
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+# Before you start
+
+## Create your own questions
+
+Each category you create has following format,
+```
+CATEGORY: TENNIS FACTS
+
+QE: Questions Content?  # Note: QE/QM/QH means easy/medium/hard and each question worth 3, 5, 8 points perspectively
+IMG: file_name  # Note: this is optional, all images needed to placed under src/assets/ folder
+A: BILLIE JEAN KING  # Note: first answer is always the correct answer, and all answers will be shuffled during the game
+A: CHRIS EVERT-LLOYD
+A: MARGARET COURT
+A: MARTINA NAVRATILOVA
+
+...
+```
+
+## Parse your questions
+```
+cd script
+python parseRound1.py {your_question_file.txt} ../src/assets/Round1.json
+```
+
+# Start the game
+```npm run serve```
+
+- Open broswer and go to localhost:8080
+
+- Enter the teams (at least two teams required)
+![alt text](TeamPage.png)
+
+- Press "Start"
+- "Next team to answer:" will choose answer by pressing the button from the page
+![alt text](Round1.png)
+- "First team to answer:" will start answer the question as follow
+![alt text](ChooseQuestion.png)
+- "Next team to answer:" will have chance to steal half of the question points by answering correctly or lose 1 point if wrong after "First team to answer:" answer wrong
+- After this question is done, "Next team to answer:" will choose another question
--- script/parseRound1.py
+++ script/parseRound1.py
@@ -1,53 +1,61 @@
 import re
 import json
 from pprint import pprint
-input_file = "Round1_2.txt"
-output_file = "Round1.json"
 
-result = []
+def parse_round1(input_file, output_file):
+    result = []
 
-with open(input_file, 'r') as fp:
-    currentCategory = None
-    currentQuestion = None
-    for line in fp:
-        m = re.match("(.*): (.*)", line)
-        if m is None:
-            continue
-        key = m.group(1)
-        statement = m.group(2)
-        if key.upper() == "CATEGORY":
-            if currentCategory is not None:
-                result.append(currentCategory.copy())
-            currentCategory = {}
-            currentCategory['name'] = statement
-            currentCategory['easy'] = []
-            currentCategory['medium'] = []
-            currentCategory['hard'] = []
-        if key.upper() in {"QE", "QM", "QH"} and currentCategory is not None:
-            currentQuestion = {}
-            currentQuestion['question'] = statement
-            currentQuestion['state'] = False
-            currentQuestion['incorrectAnswers'] = []
-            currentQuestion['img'] = ''
-            answer_match = re.match("(.*): (.*)", fp.readline())
-            count = 0
-            print(statement)
-            while answer_match is not None:
-                if count == 0 and answer_match[1] == 'IMG':
-                    currentQuestion['img'] = answer_match[2]
-                elif count == 0 and answer_match[1] == "A":
-                    currentQuestion['correctAnswer'] = answer_match[2]
-                    count += 1
-                else:
-                    currentQuestion['incorrectAnswers'].append(answer_match[2])
+    with open(input_file, 'r') as fp:
+        currentCategory = None
+        currentQuestion = None
+        for line in fp:
+            m = re.match("(.*): (.*)", line)
+            if m is None:
+                continue
+            key = m.group(1)
+            statement = m.group(2)
+            if key.upper() == "CATEGORY":
+                if currentCategory is not None:
+                    result.append(currentCategory.copy())
+                currentCategory = {}
+                currentCategory['name'] = statement
+                currentCategory['easy'] = []
+                currentCategory['medium'] = []
+                currentCategory['hard'] = []
+            if key.upper() in {"QE", "QM", "QH"} and currentCategory is not None:
+                currentQuestion = {}
+                currentQuestion['question'] = statement
+                currentQuestion['state'] = False
+                currentQuestion['incorrectAnswers'] = []
+                currentQuestion['img'] = ''
                 answer_match = re.match("(.*): (.*)", fp.readline())
-            if key.upper() == "QE":
-                currentCategory['easy'].append(currentQuestion.copy())
-            elif key.upper() == "QM":
-                currentCategory['medium'].append(currentQuestion.copy())
-            elif key.upper() == "QH":
-                currentCategory['hard'].append(currentQuestion.copy())
+                count = 0
+                print(statement)
+                while answer_match is not None:
+                    if count == 0 and answer_match[1] == 'IMG':
+                        currentQuestion['img'] = answer_match[2]
+                    elif count == 0 and answer_match[1] == "A":
+                        currentQuestion['correctAnswer'] = answer_match[2]
+                        count += 1
+                    else:
+                        currentQuestion['incorrectAnswers'].append(answer_match[2])
+                    answer_match = re.match("(.*): (.*)", fp.readline())
+                if key.upper() == "QE":
+                    currentCategory['easy'].append(currentQuestion.copy())
+                elif key.upper() == "QM":
+                    currentCategory['medium'].append(currentQuestion.copy())
+                elif key.upper() == "QH":
+                    currentCategory['hard'].append(currentQuestion.copy())
 
-result.append(currentCategory.copy())
-with open(output_file, 'w') as fp2:
-    json.dump(result, fp2, indent=2)
+    result.append(currentCategory.copy())
+    with open(output_file, 'w') as fp2:
+       json.dump(result, fp2, indent=2)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='convert question sheet to json')
+    parser.add_argument('paths', metavar='P', type=str, nargs=2,
+                    help='Input path and output path')
+    args = parser.parse_args()
+    print(args.paths)
+    parse_round1(args.paths[0], args.paths[1])
